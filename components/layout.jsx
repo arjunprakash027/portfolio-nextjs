e0d19abe8524c2fa405c0b7265e30b8965a15fb1
Arjun <arjunprakash027@gmail.com>
2024-01-03 02:51:52 +0530

    projects and blog page

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,13 +1,44 @@
-import Navbar from "./navbar";
-function Layout(props) {
-    const { children } = props;
-  
-    return(
-      <div>
-        <Navbar />
-      </div>
-    );
-  }
-  
-  export default Layout;
-  
+// components/layout.js
+import React from 'react';
+import Link from 'next/link';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-gray-100">
+      <main className="w-full md:w-1/2 p-4 md:p-8 flex flex-col flex-1 bg-white">
+        <div className="navbar flex flex-col md:flex-row justify-between items-center bg-white p-4 mb-12">
+          <div className="logo mb-4 md:mb-0">
+            <img alt="Arjun homepage" src="./logo.png" className="h-12 md:h-auto" />
+          </div>
+          <nav className="nav flex gap-4 text-2xl">
+            <Link href="/">
+              <span className="link" title="Home">
+                Home
+              </span>
+            </Link>
+
+            <Link href="/projects">
+              <span className="link" title="Projects">
+                projects
+              </span>
+            </Link>
+
+          </nav>
+        </div>
+        {children}
+        <footer className="w-full h-10 border-t border-gray-300 flex justify-center items-center">
+          <a
+            className="no-underline text-current"
+            href="https://github.com/arjunprakash027"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>Arjun Prakash &copy; 2020 - 2024</p>
+          </a>
+        </footer>
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/blogs.jsx
@@ -0,0 +1,78 @@
+import Layout from '/components/layout';
+import React from 'react';
+
+const blogdata = [
+  {
+    date: 'Aug 2023',
+    name: 'autocomp.js',
+    link: 'https://github.com/knadh/autocomp.js',
+    description: 'A super tiny Javascript autocomplete / autosuggestions library. Zero dependencies, ~800 bytes min+gzip.'
+  },
+  {
+    date: 'Aug 2023',
+    name: 'autocomp.js',
+    link: 'https://github.com/knadh/autocomp.js',
+    description: 'A super tiny Javascript autocomplete / autosuggestions library. Zero dependencies, ~800 bytes min+gzip.'
+  },
+  {
+    date: 'Aug 2023',
+    name: 'autocomp.js',
+    link: 'https://github.com/knadh/autocomp.js',
+    description: 'A super tiny Javascript autocomplete / autosuggestions library. Zero dependencies, ~800 bytes min+gzip.'
+  },
+  {
+    date: 'Aug 2023',
+    name: 'autocomp.js',
+    link: 'https://github.com/knadh/autocomp.js',
+    description: 'A super tiny Javascript autocomplete / autosuggestions library. Zero dependencies, ~800 bytes min+gzip.'
+  },
+  {
+    date: 'Aug 2023',
+    name: 'autocomp.js',
+    link: 'https://github.com/knadh/autocomp.js',
+    description: 'A super tiny Javascript autocomplete / autosuggestions library. Zero dependencies, ~800 bytes min+gzip.'
+  },
+  {
+    date: 'Aug 2023',
+    name: 'autocomp.js',
+    link: 'https://github.com/knadh/autocomp.js',
+    description: 'A super tiny Javascript autocomplete / autosuggestions library. Zero dependencies, ~800 bytes min+gzip.'
+  },
+  {
+    date: 'Aug 2023',
+    name: 'autocomp.js',
+    link: 'https://github.com/knadh/autocomp.js',
+    description: 'A super tiny Javascript autocomplete / autosuggestions library. Zero dependencies, ~800 bytes min+gzip.gfdgdfgdfgdfgdgdfgdfgdf  sgsdfgadfgasefgdf d'
+  },
+  // Add more projects as needed
+];
+
+const Blogs = () => {
+    return (
+      <Layout>
+        <div className="p-5">
+          <h2 className="text-3xl md:text-5xl text-black-500 mb-8">Projects on <a className="text-red-500" href='https://github.com/arjunprakash027'>Github</a></h2>
+          <ul className="list-disc space-y-4 leading-5 text-base">
+            {blogdata.map((blog, index) => (
+              <li key={index} className="mb-6 md:mb-8">
+                <div className="text-gray-500 text-sm md:text-2xl mb-2">
+                  {blog.date}
+                </div>
+                <div>
+                  <a href={project.link} className="text-blue-500 hover:underline text-lg md:text-2xl font-bold">
+                    {blog.name}
+                  </a>
+                </div>
+                <div className="text-gray-700 mt-2 md:text-2xl">
+                  {blog.description}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Layout>
+    );
+  };
+
+  
+export default Blogs;
